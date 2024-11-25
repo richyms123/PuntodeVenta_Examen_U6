7c7fe47a99fccf3f0838cb1862b93c90df66057c
Ricardo Moreno Salgado <richysalgado123@gmail.com>
2024-11-24 19:51:23 -0600

    Se agregó el diseño del formulario de login

---
+++ src/main/java/mycompany/puntodeventa_examen_u6/PuntodeVenta_Examen_U6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mycompany.puntodeventa_examen_u6;
+
+/**
+ *
+ * @author rickp
+ */
+public class PuntodeVenta_Examen_U6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
