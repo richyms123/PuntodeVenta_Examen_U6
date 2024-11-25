bc12ea1fc8cd088d8cc46c625f68da4c07275f02
Ricardo Moreno Salgado <richysalgado123@gmail.com>
2024-11-25 15:24:10 -0600

    Implementación de la clase Producto y Usuario sin validaciones

---
+++ src/main/java/Clases/Producto.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author rickp
+ */
+public class Producto {
+    private int clave;
+    private String nombre;
+    private float precio;
+    private int existencias;
+
+    public int getClave() {
+        return clave;
+    }
+
+    public void setClave(int clave) {
+        this.clave = clave;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public int getExistencias() {
+        return existencias;
+    }
+
+    public void setExistencias(int existencias) {
+        this.existencias = existencias;
+    }
+    
+    
+}
---
+++ src/main/java/Clases/Usuario.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author rickp
+ */
+public class Usuario {
+    private String nombre;
+    private String usuario;
+    private String password;
+    
+
+    public Usuario(String datos) {
+        String[] elementos=datos.split("°");
+        nombre=elementos[0];
+        usuario=elementos[1];
+        password= elementos[2];
+    }
+    
+    public Usuario(String nombre, String usuario, String contrasena) {
+        this.nombre = nombre;
+        this.usuario = usuario;
+        this.password = contrasena;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasena() {
+        return password;
+    }
+
+    public void setContrasena(String password) {
+        //metodo de incrptacion
+        this.password = password;
+    }
+
+}
